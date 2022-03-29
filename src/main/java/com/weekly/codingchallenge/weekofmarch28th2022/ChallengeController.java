cffb34d5432401c289d970f73773514a7dd29c74
Peter Moore <masterpm84@gmail.com>
2022-03-29 03:09:12 -0500

    Added description of the challenge for week of 3/28/22

--- src/main/java/com/weekly/codingchallenge/weekofmarch28th2022/ChallengeController.java
+++ src/main/java/com/weekly/codingchallenge/weekofmarch28th2022/ChallengeController.java
@@ -5,6 +5,18 @@ import java.util.ArrayList;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/* 100 Lockers Problem
+ * The 100 Lockers problem is a math challenge for January 2017
+ * The challenge is as follows: There are 100 lockers lined up in a hallway numbered 1 to 100
+ * one the first pass all 100 lockers are opened, on the second pass every other locker is closed 
+ * on the third pass and on every nth (n being equal to the pass number e.g. 3) 
+ * locker is toggled depending on it's state (open or closed) 
+ * The question is how many lockers are left open after 100 passes
+ * This rest controller finds out the total times the lockers were opened, 
+ * the number of lockers left opened after 100 passes, 
+ * and the lockers that are opened and returns a String with each result
+ * Results can be seen at http://localhost:8080/getLockerCount once you have cloned or downloaded the package from github */
+
 @RestController
 public class ChallengeController {
 
